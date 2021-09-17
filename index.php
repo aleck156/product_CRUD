7c09a7790a2ecd6b5e9d3578c539473c87d66e6a
Aleksander Jaworski <aleck156@gmail.com>
2021-09-17 08:28:06 +0200

    rename index page title

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Products CRUP App</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1>Products CRUP App</h1>
 
   </body>
 </html>
