d08fb9f0f49980d65dbfb86e3a0e73186b441068
Aleksander Jaworski <aleck156@gmail.com>
2021-09-17 10:50:57 +0200

    dump the $_POST, everything works fine

--- create.php
+++ create.php
@@ -2,6 +2,9 @@
   $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+  echo "<pre>";
+  var_dump($_POST);
+  echo "</pre>";
 ?>
 
 
