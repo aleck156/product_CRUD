fc040973d276bad6f5e12f1a41ce7f2821f6d5c6
Aleksander Jaworski <aleck156@gmail.com>
2021-09-17 08:49:05 +0200

    prepare sql statement

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
+  $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
 ?>
 
 
