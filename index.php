1ae8acaac023e899678f3f7c881845e9f9672f3e
Aleksander Jaworski <aleck156@gmail.com>
2021-09-17 09:05:25 +0200

    update table content with fetched data

--- index.php
+++ index.php
@@ -31,29 +31,27 @@
       <thead>
         <tr>
           <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
+          <th scope="col">Image</th>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Create Date</th>
+          <th scope="col">Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        <?php foreach($products as $i => $product): ?>
+              <tr>
+                <th scope="row"><?php echo $i+1 ?></th>
+                <td><?php $product['image'] ?></td>
+                <td><?php $product['title'] ?></td>
+                <td><?php $product['price'] ?></td>
+                <td><?php $product['create_date'] ?></td>
+                <td>
+                  <button type="button" class="btn btn-outline-primary">Edit</button>
+                  <button type="button" class="btn btn-outline-danger">Delete</button>
+                </td>
+            </tr>
+            <?php endforeach; ?>
       </tbody>
     </table>
 
