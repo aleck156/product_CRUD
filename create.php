807771b0ab768e95626994395a97df4cf4b40493
Aleksander Jaworski <aleck156@gmail.com>
2021-09-17 10:33:34 +0200

    set form fields

--- create.php
+++ create.php
@@ -22,17 +22,20 @@
 
     <form>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label>Product Image</label>
+        <input type="file" class="form-control">
       </div>
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <label>Product Title</label>
+        <input type="text" class="form-control">
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      <div class="mb-3">
+        <label>Product Description</label>
+        <textarea type="text" class="form-control"></textarea>
+      </div>
+      <div class="mb-3">
+        <label>Product Price</label>
+        <input type="number" step="0.01" class="form-control" value="0.00" min="0.00">
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
