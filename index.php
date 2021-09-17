ebf04ce53bb478cf744a93539fcf326f8b889c83
Aleksander Jaworski <aleck156@gmail.com>
2021-09-17 08:28:30 +0200

    fix typo in index page title

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
-    <title>Products CRUP App</title>
+    <title>Products CRUD App</title>
   </head>
   <body>
-    <h1>Products CRUP App</h1>
+    <h1>Products CRUD App</h1>
 
   </body>
 </html>
