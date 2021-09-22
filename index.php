93d2a93690983301d9d4415d3f1eace10818177d
Aleksander Jaworski <aleck156@gmail.com>
2021-09-22 08:50:09 +0200

    create select statement for search bar

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
   $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+  $search = $_GET['search'] ?? '';
+
+  if ($search) {
+    $statement = $pdo->prepare('SELECT * FROM products WHERE title LIKE :search ORDER BY create_date DESC');
+    $statement->bindValue(':search', "%$search%");
+  } else {
+    $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+  }
 
   $statement->execute();
   $products = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -33,7 +40,8 @@
 
     <form>
       <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Search for products ..." name="search">
+        <input type="text" class="form-control" placeholder="Search for products ..." name="search"
+          value="<?php echo $search; ?>">
         <button class="btn btn-outline-secondary" type="submit">
           <i class="bi bi-search"></i>
         </button>
